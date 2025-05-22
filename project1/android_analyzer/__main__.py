a7a61ea199b3faa260b86d1c3c7065b89327ca9e
Craig Tieman <craig.tieman2007@gmail.com>
2025-05-21 23:08:45 -0400

    Add command line arguments. Add ADB command to obtain network data information and associate that information to packages

--- project1/android_analyzer/__main__.py
+++ project1/android_analyzer/__main__.py
@@ -4,7 +4,8 @@ import psutil
 import subprocess
 import time
 
-from .adb_utils import get_packages, get_pkg_uid
+from .adb_utils import get_net_data, get_packages, get_pkg_uid
+from .package import PackageData
 from ppadb.client_async import ClientAsync as AdbClient
 
 MAX_WAIT_TIME = 10
@@ -12,7 +13,15 @@ MAX_WAIT_TIME = 10
 async def main():
     adb = None
     device = None
-    
+
+    parser = argparse.ArgumentParser(
+         prog="android_analyer",
+         description="android-analyzer is a Python library to help with viewing processes and packages on an Android device to help with identifying packages that are using more data than they should or are displaying unusual or suspicious behavior.",
+    )
+    parser.add_argument("-d", "--device", help="The serial number of the Android device to be analyzed", default=None)
+    parser.add_argument("-o", "--output", help="File to output results to")
+    args = parser.parse_args()
+
     # Start ADB if it is not running
     if "adb" not in (p.name() for p in psutil.process_iter()):
         print("Starting ADB")
@@ -30,15 +39,17 @@ async def main():
     client = AdbClient(host="127.0.0.1", port=5037)
     
     # Select the device analyze
-    devices = await client.devices()
-    print("Select from the following devices:")
-    for i in range(0, len(devices)):
-        print(f"\t{i}) {devices[i].serial}")
-    try:
-        selection = int(input("Selection: "))
-        device = devices[selection]
-    except:
-        print("Invalid selection")
+    device = await client.device(args.device)
+    if device == None:
+        devices = await client.devices()
+        print("Select from the following devices:")
+        for i in range(0, len(devices)):
+            print(f"\t{i}) {devices[i].serial}")
+        try:
+            selection = int(input("Selection: "))
+            device = devices[selection]
+        except:
+            print("Invalid selection")
     if device == None:
         print("ERROR: No device selected")
         raise Exception("No device selected")
@@ -52,8 +63,18 @@ async def main():
     print("Getting UIDs")
     tasks = [get_pkg_uid(device, pkg) for pkg in pkg_list]
     results = await asyncio.gather(*tasks)
-    results_dict = dict(results)
-    print(results_dict)
+    for result in results:
+        print(f"Package: {result.pkg}, UID: {result.uid}")
+
+    # Get network data usage
+    netstat_list = await get_net_data(device)
+    for netstat_data in netstat_list:
+        for pkg in pkg_list:
+            if len(netstat_data) == 5 and netstat_data[0] == pkg.uid:
+                pkg.data_rx = netstat_data[1]
+                pkg.data_tx = netstat_data[3]
+    for pkg in pkg_list:
+        pkg.print()
 
     # Close ADB if it was started
     if adb != None:
--- project1/android_analyzer/adb_utils.py
+++ project1/android_analyzer/adb_utils.py
@@ -1,21 +1,37 @@
 import asyncio
 
+from .package import PackageData
+
 async def get_packages(device):
     pkg_list = []
     result = await device.shell("cmd package list packages")
     tmp = result.split("\n")
     for pkg in tmp:
         if len(pkg) > 8 and pkg[:8] == "package:":
-            pkg_list.append(pkg[8:])
+            pkg_list.append(PackageData(pkg[8:]))
     return pkg_list
 
 async def get_pkg_uid(device, pkg):
-    tmp = await device.shell(f"dumpsys package {pkg} | grep appId")
+    tmp = await device.shell(f"dumpsys package {pkg.pkg} | grep appId")
     uid = tmp[tmp.find("appId"):]
     if len(uid) > 6:
-        uid = uid[6:].split(" ")[0].strip()
-    return uid , pkg
-    
+        pkg.uid = uid[6:].split(" ")[0].strip()
+    return pkg
+
+async def get_net_data(device):
+    net_data = []
+    tmp = await device.shell("dumpsys netstats")
+    start = tmp.find("mAppUidStatsMap:\n")
+    if start > -1:
+        lines = tmp[start:].split("\n")
+    if len(lines) > 1:
+        lines = lines[1:]
+    for line in lines:
+        if len(line) < 4 or line[:4] != "    ":
+            break
+        net_data.append(line.strip().split(" "))
+    return net_data
+
 async def get_running_processes(device):
     processes = await device.shell("ps -A")
     return processes
